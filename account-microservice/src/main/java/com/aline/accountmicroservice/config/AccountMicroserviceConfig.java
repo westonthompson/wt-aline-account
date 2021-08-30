0a9305ae30aa93721d6ab7a628ce892b6ee7e184
leandroyabut <leandro.yabut@smoothstack.com>
2021-08-29 22:32:24 -0600

    AF-256 Remove enable web mvc.

--- account-microservice/src/main/java/com/aline/accountmicroservice/config/AccountMicroserviceConfig.java
+++ account-microservice/src/main/java/com/aline/accountmicroservice/config/AccountMicroserviceConfig.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
-@EnableWebMvc
 public class AccountMicroserviceConfig {
 
     @Bean
