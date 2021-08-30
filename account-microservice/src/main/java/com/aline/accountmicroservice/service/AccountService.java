4a4fbc67b4eec2121112af3817806e70f71650b0
leandroyabut <leandro.yabut@smoothstack.com>
2021-08-29 19:29:45 -0600

    AF-256 Add get account by id.

---
+++ account-microservice/src/main/java/com/aline/accountmicroservice/service/AccountService.java
@@ -0,0 +1,38 @@
+package com.aline.accountmicroservice.service;
+
+import com.aline.core.dto.response.AccountResponse;
+import com.aline.core.exception.notfound.AccountNotFoundException;
+import com.aline.core.model.account.Account;
+import com.aline.core.repository.AccountRepository;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AccountService {
+
+    private final AccountRepository repository;
+    private final ModelMapper mapper;
+
+    /**
+     * Get an account by its primary key.
+     * @param id The primary key of the account
+     * @return The account with the specified primary key.
+     */
+    public Account getAccountById(long id) {
+        return repository.findById(id).orElseThrow(AccountNotFoundException::new);
+    }
+
+
+    /**
+     * Map account entity to AccountResponse DTO.
+     * @param account The account entity to be mapped
+     * @return An AccountResponse dto mapped from the passed account entity.
+     */
+    public AccountResponse mapToResponse(Account account) {
+        return mapper.map(account, AccountResponse.class);
+    }
+
+
+}
---
+++ account-microservice/src/test/java/com/aline/accountmicroservice/service/AccountServiceTest.java
@@ -0,0 +1,56 @@
+package com.aline.accountmicroservice.service;
+
+import com.aline.core.annotation.test.SpringBootUnitTest;
+import com.aline.core.model.account.Account;
+import com.aline.core.model.account.AccountStatus;
+import com.aline.core.model.account.AccountType;
+import com.aline.core.model.account.CheckingAccount;
+import com.aline.core.repository.AccountRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.when;
+
+@SpringBootUnitTest
+public class AccountServiceTest {
+
+    @Autowired
+    AccountService accountService;
+
+    @MockBean
+    AccountRepository repository;
+
+    @BeforeEach
+    void setUp() {
+        Account account = CheckingAccount.builder()
+                .id(1L)
+                .accountNumber("123456789")
+                .balance(10000)
+                .availableBalance(10000)
+                .status(AccountStatus.ACTIVE)
+                .build();
+        when(repository.findById(1L))
+                .thenReturn(Optional.of(account));
+    }
+
+    @Test
+    void test_getAccountById_returns_the_correctAccount() {
+
+        Account account = accountService.getAccountById(1L);
+        assertNotNull(account);
+        assertEquals(1L, account.getId());
+        assertEquals("123456789", account.getAccountNumber());
+        assertEquals(AccountType.CHECKING, account.getAccountType());
+        assertEquals(10000, account.getBalance());
+        assertTrue(account instanceof CheckingAccount);
+
+    }
+
+}
